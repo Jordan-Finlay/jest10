e2b44b019d4fbac328a3bf29b8289638759a0b56
Jordan Finlay <jfinlay@hotmail.co.uk>
2024-03-19 10:44:32 +0000

    updated button js and calc js

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,23 @@
+
+
+const addition = require("../calc");
+
+describe("calcultor", () => {
+    describe("addition function", () => {
+        test("should return 42 for 22 + 20", () => {
+            expect(addition(20, 22)).toBe(42);
+        });
+        test("should return 73 for 42 + 31", () => {
+            expect(addition(42, 31)).toBe(73);
+        });
+    });
+    describe("subtraction function", () => {
+
+    });
+    describe("multiply function", () => {
+
+    });
+    describe("division function", () => {
+
+    });
+})
